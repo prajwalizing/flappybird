48c71e1dcba2e0c3a1f4a7185e10526aec1b3378
prajwalhs-official <prajwalhs.official@gmail.com>
2026-09-05 18:40:21 +0530

    optimised difficulty level gap & deleted design file

--- app/src/main/java/com/prajwalhs/flappybird/domain/model/Difficulty.kt
+++ app/src/main/java/com/prajwalhs/flappybird/domain/model/Difficulty.kt
@@ -1,7 +1,7 @@
 package com.prajwalhs.flappybird.domain.model
 
 enum class Difficulty(val pipeGapHeight: Float, val pipeSpeed: Float) {
-    CHILL(pipeGapHeight = 250f, pipeSpeed = 130f),
-    CLASSIC(pipeGapHeight = 210f, pipeSpeed = 158f),
-    STORM(pipeGapHeight = 190f, pipeSpeed = 176f)
+    CHILL(pipeGapHeight = 300f, pipeSpeed = 130f),
+    CLASSIC(pipeGapHeight = 250f, pipeSpeed = 158f),
+    STORM(pipeGapHeight = 210f, pipeSpeed = 176f)
 }
